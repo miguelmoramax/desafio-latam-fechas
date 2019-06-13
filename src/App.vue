58a66b6442937c152fe277fc90e9e3a3e0c6e4f5
miguelmoramax <miguelmoramaturana@gmail.com>
2019-06-12 20:39:00 -0400

    fix indent

--- src/App.vue
+++ src/App.vue
@@ -26,76 +26,75 @@ function formatNumber(num) {
 }
 
 export default {
-  name: 'app',
-  data() {
-      return {
-        img:'http://blog.desafiolatam.com/wp-content/uploads/2015/03/desafio-latam-logonegro.png',
-        selectedTime: 0,
-        timeLeft: '00:00:00',
-        endTime: '0',
-        times: [
-            {
-                sec: 3,
-                display: '3s'
-            },
-            {
-                sec: 60,
-                display: '1m'
-            },
-            {
-                sec: 300,
-                display: '5m'
-            },
-            {
-                sec: 600,
-                display: ' 10m'
-            },
-            {
-                sec: 1800,
-                display: '30m'
-            }
-        ]
+name: 'app',
+data() {
+return {
+  img:'http://blog.desafiolatam.com/wp-content/uploads/2015/03/desafio-latam-logonegro.png',
+  selectedTime: 0,
+  timeLeft: '00:00:00',
+  endTime: '0',
+  times: [
+        {
+            sec: 3,
+            display: '3s'
+        },
+        {
+            sec: 60,
+            display: '1m'
+        },
+        {
+            sec: 300,
+            display: '5m'
+        },
+        {
+            sec: 600,
+            display: ' 10m'
+        },
+        {
+            sec: 1800,
+            display: '30m'
+        }
+    ]
     }
   },
-  methods: {
-    setTime(seconds) {
-      clearInterval(intervalTimer);
-      this.timer(seconds);
-    },
-    timer(seconds) {
-      const now = Date.now();
-      const end = now + seconds * 1000;
-      this.displayTimeLeft(seconds);
+    methods: {
+        setTime(seconds) {
+        clearInterval(intervalTimer);
+        this.timer(seconds);
+        },
+        timer(seconds) {
+        const now = Date.now();
+        const end = now + seconds * 1000;
+        this.displayTimeLeft(seconds);
 
-      this.selectedTime = seconds;
-      this.initialTime = seconds;
-      this.countdown(end);
-    },
-    countdown(end) {
-      this.initialTime = this.selectedTime;
-      intervalTimer = setInterval(() => {
-        const secondsLeft = Math.round((end - Date.now()) / 1000);
+        this.selectedTime = seconds;
+        this.initialTime = seconds;
+        this.countdown(end);
+        },
+        countdown(end) {
+        this.initialTime = this.selectedTime;
+        intervalTimer = setInterval(() => {
+            const secondsLeft = Math.round((end - Date.now()) / 1000);
 
-        if(secondsLeft === 0) {
-          this.endTime = 0;
-        }
+            if(secondsLeft === 0) {
+            this.endTime = 0;
+            }
 
-        if(secondsLeft < 0) {
-          clearInterval(intervalTimer);
-          return;
-        }
-        this.displayTimeLeft(secondsLeft)
-      }, 1000);
-    },
-    displayTimeLeft(secondsLeft) {
-      const minutes = Math.floor((secondsLeft % 3600) / 60);
-      const seconds = secondsLeft % 60;
+            if(secondsLeft < 0) {
+            clearInterval(intervalTimer);
+            return;
+            }
+            this.displayTimeLeft(secondsLeft)
+        }, 1000);
+        },
+        displayTimeLeft(secondsLeft) {
+        const minutes = Math.floor((secondsLeft % 3600) / 60);
+        const seconds = secondsLeft % 60;
 
-      this.timeLeft = `${formatNumber(minutes)}:${formatNumber(seconds)}`;
-    },
-  }
-
-    };
+        this.timeLeft = `${formatNumber(minutes)}:${formatNumber(seconds)}`;
+        },
+    }
+};
 </script>
 
 <style scoped>
@@ -146,6 +145,4 @@ export default {
         border-radius: 0;
         background-color: lightgray;
     }
-
-
-    </style>
+</style>
